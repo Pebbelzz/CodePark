86571d6ab7fce800bfffc0a9c653187bdd8fb43d
Pebbelzz <greeneyes14j@gmail.com>
2015-05-13 10:42:22 -0400

    adding drag

---
+++ Diddle/script.js
@@ -0,0 +1,24 @@
+	//makes #notready fade out when mouse goes over it
+	$(document).ready(function(){
+    $("#notready").mouseenter(function() {
+        $("#notready").fadeOut(0);
+    });
+
+});
+	
+	//makes #clickme fadeout upon loading of screen
+	$(document).ready(function() {
+    $("#clickme").fadeOut(2000);
+});
+
+$(document).ready(function() {
+	$("#fadeclick").click(function(){
+		$("#fadeclick").fadeOut(3000);
+	});
+});
+
+$(document).ready(function(){
+	$('button').click(function(){
+		$('.center').fadeIn(1000);
+	});
+});
