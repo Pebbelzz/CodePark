8d9303b8e46b6beec9924cdeff86db04463879f8
Pebbelzz <greeneyes14j@gmail.com>
2015-05-21 17:55:28 -0400

    added clock

---
+++ clock/script.js
@@ -0,0 +1,16 @@
+var d = new Date();
+var hours = d.getHours();
+var minutes = d.getMinutes();
+console.log(hours);
+
+function timeHours(){
+   document.getElementById("hour").innerHTML = hours;
+      };
+
+function timeMin(){
+   document.getElementById("minutes").innerHTML = minutes;
+      };
+
+
+
+
